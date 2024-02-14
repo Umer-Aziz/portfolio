06244f2cea278a3cf04c51bc5ee4f52e6b182d64
Umer-Aziz <umars25271997@gmail.com>
2024-02-14 15:55:31 +0500

    added footer  section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,11 +13,11 @@ const Footer = () => {
         <div className=" padd-x bg-neutral-900 dark:bg-opacity-50">
         <div className="w-full relative antialiased text-white">
       <div className="max-w-2xl mx-auto flex flex-col items-center p-4">
-         <p className='mt-4 z-10 text-center'>I CRAFT DIGITAL EXPERIENCES WITH CREATIVE FLAIR</p>
+         <p className='mt-4 z-10 text-center text-sm'>I CRAFT DIGITAL EXPERIENCES WITH CREATIVE FLAIR</p>
          <div className='-translate-y-4 z-10'>
             <SocialShare/>
          </div>
-         <p>&copy; All right reserved by umeraziz - 2024</p>
+         <p className='text-sm'>&copy; All right reserved by umeraziz - 2024</p>
       </div>
       <BackgroundBeams />
     </div>

39939228797d7da24e30947c301a00c39905420a
Umer-Aziz <umars25271997@gmail.com>
2024-02-17 20:34:21 +0500

    updated reviews section

--- components/home/ReviewSlider.tsx
+++ components/home/ReviewSlider.tsx
@@ -24,29 +24,40 @@ import {
 export default function ReviewSlider() {
     const SliderData = [
         {
-            Image: "https://github.com/shadcn.png",
-            Title: "Thomas Shelby",
-            subTitle:"Actor",
-            Desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, assumenda beatae? Facere, asperiores quam! A cupiditate ut animi dignissimos consequuntur adipisci sed reprehenderit sunt?"    
+            Image: "/img/avatar-3.png",
+            Title: "Daniel Harris",
+            fallback:"DH",
+            subTitle:"SkyStudio Co Founder",
+            Desc:"As co-founder, I highly commend Mr. Umer for his exceptional dedication, expertise, and collaborative spirit. With his keen attention to detail and proactive problem-solving approach, he significantly elevates our team dynamic."
+        }, 
+        {
+            Image: "/img/abbir.jpg",
+            Title: "Abbir Ali",
+            fallback:"AB",
+            subTitle:"Intern colleague",
+            Desc: "Working with Umer was fantastic! Their expertise in React.js and dedication to quality made them invaluable. Efficient, skilled, and a pleasure to collaborate with – highly recommend for any project!"    
+        }, 
+        {
+            Image: "/img/avatar-5.png",
+            Title: "Muhammad Hamza ",
+            fallback:"MH",
+            subTitle:"President SES",
+            Desc: "Working alongside Mr Umer has been a privilege.Their dedication and leadership have greatly enhanced our society. Their commitment to fostering growth has been instrumental in achieving our goals."    
+        }, 
+        {
+            Image: "/img/avatar-4.png",
+            Title: "Ekta Mirpuri",
+            fallback:"EM",
+            subTitle:"Upwork Client", 
+            Desc: "I have had an absolute great experience working with Umer! He is prompt, timely, gives good suggestions and gets the job done excellently with any assistance required otherwise as well! Couldn't recommend him more highly than this!."    
         },  
         {
-            Image: "https://github.com/shadcn.png",
-            Title: "Thomas Shelby",
-            subTitle:"Actor",
-            Desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, assumenda beatae? Facere, asperiores quam! A cupiditate ut animi dignissimos consequuntur adipisci sed reprehenderit sunt?"    
-        },  
-        {
-            Image: "https://github.com/shadcn.png",
-            Title: "Thomas Shelby",
-            subTitle:"Actor",
-            Desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, assumenda beatae? Facere, asperiores quam! A cupiditate ut animi dignissimos consequuntur adipisci sed reprehenderit sunt?"    
-        },  
-        {
-            Image: "https://github.com/shadcn.png",
-            Title: "Thomas Shelby",
-            subTitle:"Actor",
-            Desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, assumenda beatae? Facere, asperiores quam! A cupiditate ut animi dignissimos consequuntur adipisci sed reprehenderit sunt?"    
-        },  
+            Image: "/img/avatar-6.png",
+            Title: "RushFone",
+            fallback:"MA",
+            subTitle:"Upwork Client",
+            Desc: "Umer did the task perfectly and efficiently, exceeding our expectations. Now the site looks highly professional and polished. Thank you for completing the task quickly. Highly recommend for future projects!"    
+        },     
         
     ]
     return (
@@ -89,7 +100,7 @@ export default function ReviewSlider() {
                                     <div className='flex items-center gap-3'>
                                     <Avatar>
                                         <AvatarImage src={data.Image} alt="user" />
-                                        <AvatarFallback>CN</AvatarFallback>
+                                        <AvatarFallback>{data.fallback}</AvatarFallback>
                                     </Avatar>
                                     <div className='mt-1'>
                                         <CardTitle>{data.Title}</CardTitle>
